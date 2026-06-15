5bc274258b839a55c852e7f03a1fc1d600ccb641
Gleb Ustimenko <thebrianmaps142@gmail.com>
2026-06-15 18:32:27 +0500

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -12,7 +12,7 @@ for (let i = 0; i < 256; ++i) {
 }
 
 function isValidUUID(uuid) {
-	const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+	const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 	return uuidRegex.test(uuid);
 }
 
