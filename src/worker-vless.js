5312537fea5aac5d38430cc4ab1aff337ea9d0d1
Gleb Ustimenko <thebrianmaps142@gmail.com>
2026-06-15 18:36:17 +0500

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -12,8 +12,9 @@ for (let i = 0; i < 256; ++i) {
 }
 
 function isValidUUID(uuid) {
-	const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-	return uuidRegex.test(uuid);
+	// const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+	// return uuidRegex.test(uuid);
+	return true;
 }
 
 if (!isValidUUID(DEFAULT_USER_ID)) {
